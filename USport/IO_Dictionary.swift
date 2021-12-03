c0deb8669880258160ea5c84c2cecc835bd34576
Alessandro Ferrara <alessandro.ferrara001@studenti.uniparthenope.it>
2021-12-03 15:03:12 +0100

    Add IO_Dictionary

---
+++ USport/IO_Dictionary.swift
@@ -0,0 +1,39 @@
+//
+//  IO_Dictionary.swift
+//  USport
+//
+//  Created by Alessandro Ferrara on 03/12/21.
+//
+
+import Foundation
+
+class IO_Dictionary
+{
+    private var path_dictionary : String
+    {
+        get
+        {
+            return self.path_dictionary
+        }
+        
+        set(newValue_path_dictionary)
+        {
+            self.path_dictionary = newValue_path_dictionary
+        }
+    }
+    
+    init(newValue_path_dictionary : String)
+    {
+        self.path_dictionary = newValue_path_dictionary
+    }
+    
+    func read()
+    {
+        
+    }
+    
+    func write()
+    {
+        
+    }
+}
