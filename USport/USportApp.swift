ccc3477b45c7794621d1a0b3170253c3498a70bd
Angelo Casolaro <angelo.casolaro001@studenti.uniparthenope.it>
2021-12-03 09:27:22 +0100

    Initial Commit

---
+++ USport/USportApp.swift
@@ -0,0 +1,17 @@
+//
+//  USportApp.swift
+//  USport
+//
+//  Created by Angelo Casolaro on 03/12/21.
+//
+
+import SwiftUI
+
+@main
+struct USportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
