a47a7a5f4106598d2fb0acb464153fea9b4b87b3
Ciro Testa <testaciro96@live.it>
2021-12-03 14:33:33 +0100

    Added file swift " Tennis, Volleyball, Basket" and class

---
+++ USport/Volleyball.swift
@@ -0,0 +1,20 @@
+//
+//  Volleyball.swift
+//  USport
+//
+//  Created by Ciro Testa on 03/12/21.
+//
+
+import Foundation
+
+
+class Volleyball : Sport
+{
+    override func real_CoeffSport()
+    {
+        self.Coeff_Kcal_Low = 7
+        self.Coeff_Kcal_Medium = 7.5
+        self.Coeff_Kcal_High = 8
+        self.Img_Icon = "Volleyball_icon"
+    }
+}
