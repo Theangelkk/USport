fb5fa8a368f24a0b8ef22e7b17744c2def7042a3
Ciro Testa <testaciro96@live.it>
2021-12-03 14:34:42 +0100

    added file swift "Basket,Tennis"

--- USport/Tennis.swift
+++ USport/Tennis.swift
@@ -1,5 +1,5 @@
 //
-//  Basket.swift
+//  Tennis.swift
 //  USport
 //
 //  Created by Ciro Testa on 03/12/21.
@@ -7,13 +7,14 @@
 
 import Foundation
 
-class Basket : Sport
+
+class Tennis : Sport
 {
     override func real_CoeffSport()
     {
         self.Coeff_Kcal_Low = 7
         self.Coeff_Kcal_Medium = 7.5
         self.Coeff_Kcal_High = 8
-        self.Img_Icon = "Basket_icon"
+        self.Img_Icon = "Tennis_icon"
     }
 }
