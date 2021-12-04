4ed90e6233a3bf7305d2ba369145b525010227bb
Raffaele Calcagno <raffaele.calcagno001@studenti.uniparthenope.it>
2021-12-04 10:51:20 +0100

    Loading...

--- USport/EditWorkout.swift
+++ USport/EditWorkout.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct EditWorkout: View {
     @State private var workout: String = ""
-    private var day = ["Lunedi", "Martedi"]
+    private var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    @State private var IntensityOfLevel = ["Low", "Medium", "High"]
     @State private var daySelected = 0
+    @State private var intensitySelected = 0
     @State private var start = Date()
     @State private var end = Date()
     
@@ -18,24 +20,36 @@ struct EditWorkout: View {
         NavigationView {
         GeometryReader{ geometry in
             Form{
-                Section {
+                Section(header: Text("")) {
                 TextField("Workout", text: $workout)
-
-                    
+                        
+                }.navigationBarTitle(Text("Volleyball"), displayMode: .inline)
+                
+                Section(header: Text("Actions")) {
                 Picker(selection: $daySelected, label: Text("Selected day")){
                     
                     ForEach(0 ..< day.count){
                         
                         Text(self.day[$0])
-                        }
-                }
+                    }.navigationBarTitle(Text("Day"), displayMode: .inline)
+                        
+                } 
                 
-                    
-                
-                DatePicker("Start", selection: $start, displayedComponents: .hourAndMinute)
+                    DatePicker("Start", selection: $start, displayedComponents: .hourAndMinute)
                 DatePicker("End", selection: $end, displayedComponents: .hourAndMinute)
+                    
+                    Picker(selection: $intensitySelected, label: Text("Intensity of Level")){
+                        
+                        ForEach(0 ..< IntensityOfLevel.count){
+                            
+                            Text(self.IntensityOfLevel[$0])
+                        } .navigationBarTitle(Text("Intensity of Level"), displayMode: .inline)
+                    }
                 }
-            } .navigationBarTitle(Text("day"), displayMode: .inline)
+                .navigationBarTitle(Text("Volleyball"), displayMode: .inline)
+            } .accentColor(.red)
+
+                
         }
     }
 }
