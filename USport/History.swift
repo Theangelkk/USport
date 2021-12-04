6ed286f2e1077cf6b3c7707a55430df767383fda
Alessandro Ferrara <alessandro.ferrara001@studenti.uniparthenope.it>
2021-12-04 14:33:13 +0100

    Add History

---
+++ USport/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  USport
+//
+//  Created by Alessandro Ferrara on 04/12/21.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct History_Previews: PreviewProvider {
+    static var previews: some View {
+        History()
+    }
+}
