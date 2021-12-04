94f1ea45dcc3843b7e472d29dbaaba7bb9d9efb7
Alessandro Ferrara <alessandro.ferrara001@studenti.uniparthenope.it>
2021-12-04 14:31:59 +0100

    Add Homepage

---
+++ USport/Homepage.swift
@@ -0,0 +1,20 @@
+//
+//  Homepage.swift
+//  USport
+//
+//  Created by Alessandro Ferrara on 04/12/21.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Homepage_Previews: PreviewProvider {
+    static var previews: some View {
+        Homepage()
+    }
+}
