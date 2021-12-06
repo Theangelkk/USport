8f7432a80f4d1bfb4a589be359c9989d874fa5c4
Alessandro Ferrara <alessandro.ferrara001@studenti.uniparthenope.it>
2021-12-06 17:32:08 +0100

    Add EditActivity

---
+++ USport/EditActivity.swift
@@ -0,0 +1,20 @@
+//
+//  EditActivity.swift
+//  USport
+//
+//  Created by Alessandro Ferrara on 06/12/21.
+//
+
+import SwiftUI
+
+struct EditActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditActivity_Previews: PreviewProvider {
+    static var previews: some View {
+        EditActivity()
+    }
+}
